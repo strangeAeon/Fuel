c8efa129aa69545e19d031a2161afb3db56d9334
Woramet Muangsiri <krw127@hotmail.com>
2017-11-11 18:28:10 +0900

    Introduce ReusableInputStream

---
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/util/ReusableInputStream.kt
@@ -0,0 +1,36 @@
+package com.github.kittinunf.fuel.util
+
+import java.io.BufferedInputStream
+import java.io.InputStream
+
+/**
+ * Created by woramet on 11/11/17.
+ */
+
+class ReusableInputStream(var inputStream: InputStream): InputStream() {
+
+    init {
+        if (!inputStream.markSupported()) {
+            inputStream = BufferedInputStream(inputStream)
+            throw IllegalArgumentException("Marking not supported")
+        }
+
+        inputStream.mark(Int.MAX_VALUE)
+    }
+
+    override fun read(): Int {
+        return inputStream.read()
+    }
+
+    override fun read(b: ByteArray?): Int {
+        return inputStream.read(b)
+    }
+
+    override fun read(b: ByteArray?, off: Int, len: Int): Int {
+        return inputStream.read(b, off, len)
+    }
+
+    override fun close() {
+        inputStream.reset()
+    }
+}
